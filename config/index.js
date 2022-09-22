ed4dcdeea3a080c1b499c8a4a6514fa56c412123
FH597 <93877394+FH597@users.noreply.github.com>
2022-09-22 20:01:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oDU3L5-aQ1OEBWSM08XhiymKcBdY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Jqd7UFwVUIT5MyuXJjcOdKvRp--Rowh8UZSBeke1h3M",
+      useTemplateId: "hKPQcxzI-zX7GnDq3ozMjmayH2RjqFAD3XeagPLPWbM",
       // 所在省份
       province: "贵州",
       // 所在城市
